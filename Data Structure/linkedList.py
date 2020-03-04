a16ec8e39f0f8d0a979b66e353f3c558e0627a9e
Jefferson Sousa <jefferson.sousa@imaginebeyond.com.br>
2020-03-04 20:25:03 -0300

    ksks

---
+++ Data Structure/linkedList.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        sef.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
