34223beecb66f9f92a4d02ee97f66399d9b19b10
Jefferson Sousa <jefferson.sousa@imaginebeyond.com.br>
2020-03-06 19:54:08 -0300

    new code

--- LeetCode/HouseRobber.py
+++ LeetCode/HouseRobber.py
@@ -1,20 +1,22 @@
 class Solution:
     def rob(self, nums):
-        total1 = 0
-        total2 = 0
-
-        for i in range(0,len(nums), 2):
-            total1+=nums[i]
-            if i+1 < len(nums):
-                print(len(nums))
-                total2 += nums[i+1]
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
         
-        if total1 > total2:
-            return total1
-        else:
-            return total2
+        if len(nums) == 2:
+            return max(nums[1], nums[0])
+        
+        aux_robber = [0] * len(nums)
+        aux_robber[0] = nums[0]
+        aux_robber[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            aux_robber[i] = max(nums[i]+aux_robber[i-2], aux_robber[i-1])
+        return aux_robber[-1]
+
 
 if __name__ == "__main__":
-    print(Solution().rob([1,3,1]))
+    print(Solution().rob([1,3, 1]))
     pass
         
