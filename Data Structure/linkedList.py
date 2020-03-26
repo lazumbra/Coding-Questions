504ab53c9500880ecb759c884ce5757ad7c66b76
Jefferson Sousa <lazumbra@gmail.com>
2020-03-25 22:59:20 -0300

    add delete functions to linked list

--- Data Structure/linkedList.py
+++ Data Structure/linkedList.py
@@ -44,7 +44,50 @@ class LinkedList:
 
         newNode.next = prev_node.next
         prev_node.next = newNode
+
+    def delete_node(self, key):
+
+        current_node = self.head
+
+        if current_node.data == key:
+            self.head = current_node.next
+            current_node = None
+            return   
+
+        prev = None
+        while current_node.data != key:
+            prev = current_node
+            current_node = current_node.next
+
+        if current_node is None:
+            return    
         
+        prev.next = current_node.next
+        current_node = None
+
+    def delete_node_at_pos(self, pos):
+        cur_node = self.head
+
+        if pos == 0:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        count = 0
+        prev = None
+        while count != pos:
+            prev = cur_node
+            cur_node = cur_node.next
+            count +=1
+        
+        if cur_node == None:
+            return
+        
+        prev.next = cur_node.next
+        cur_node = None
+
+
+
 
 
 llist = LinkedList()
@@ -52,6 +95,11 @@ llist.append("A")
 llist.append('B')
 llist.append('C')
 llist.append('D')
+
+#llist.delete_node("B")
+#llist.delete_node_at_pos(1)
+llist.print_list()
+print('\n')
 
 llist.insert_after_node(llist.head.next, 'E')
 llist.print_list()
