6542449597e8a4531729325b792b6e0d2d1b9256
Jefferson Martins <jefferson.martins.de.sousa@volvocars.com>
2024-04-11 16:53:06 +0200

    new code

--- checkio/q04.py
+++ checkio/q04.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 """
 https://py.checkio.org/en/mission/convert-date/
 
@@ -46,17 +48,24 @@ def convert_date(date: str) -> str:
     return f"{year:04d}-{month:02d}-{day:02d}"
 
 
+def convert_date_v2(date: str) -> str:
+    try:
+        return datetime.strptime(date, "%d/%m/%Y").strftime("%Y-%m-%d")
+    except ValueError:
+        return "Error: Invalid date."
+
+
 # These "asserts" are used for self-checking
 assert convert_date("25/12/2021") == "2021-12-25"
 assert convert_date("01/01/2000") == "2000-01-01"
-assert convert_date("15/06/1995") == "1995-06-15"
+assert convert_date_v2("15/06/1995") == "1995-06-15"
 assert convert_date("29/02/2020") == "2020-02-29"
 assert convert_date("10/10/2010") == "2010-10-10"
 assert convert_date("31/05/1985") == "1985-05-31"
 assert convert_date("07/08/1960") == "1960-08-07"
 assert convert_date("02/09/1999") == "1999-09-02"
 assert convert_date("30/04/1975") == "1975-04-30"
-assert convert_date("29/02/2019") == "Error: Invalid date."
+assert convert_date_v2("29/02/2019") == "Error: Invalid date."
 assert convert_date("30/04/1975/1") == "Error: Invalid date."
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
